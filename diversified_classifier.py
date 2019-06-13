b11453c0496e2041751f50c608bce3134af56d60
mbanaszkiewicz <baan616@gmail.com>
2019-06-13 16:07:10 +0200

    div classifier implementation std classifier removed main rewritten to use crossvalidation

--- diversified_classifier.py
+++ diversified_classifier.py
@@ -8,64 +8,60 @@ from sklearn.preprocessing import StandardScaler
 
 
 class DiversifiedClassifier:
+    classifiers_group = []
+    precision_scores = []
 
     def __init__(self, estimator, n_splits):
-        self.estimator = estimator
+        self.estimator = estimator.__class__ #store class to later create new instances of this estimator
         self.n_splits = n_splits
 
-    def predict(self, data):
-        X = data[:, :-1]
-        y = data[:, -1]
-        # X = self.pre_process_data(X)
-
-        prediction_vectors, p_scores = self.estimate_individual_predictions(X, y)
-        print(len(prediction_vectors))
-        max_size = self.get_max_size(prediction_vectors)
-        decision_data_sets = self.accumulate_predictions(prediction_vectors, p_scores, max_size)
-        results = self.calculate_final_decisions(decision_data_sets)
-        print(
-
-        )
-        print(len(y))
-        return precision_score(y, results)
-
-    def estimate_individual_predictions(self, X, y):
+    def fit(self, X, y):
+        self.classifiers_group.clear()
+        self.precision_scores.clear()
         folds = KFold(n_splits=self.n_splits)
-        prediction_vectors = []
-        p_scores = []
 
-        for train_index, test_index in folds.split(X):  # first folding
+        for train_index, test_index in folds.split(X): #for each fold create new clf, train it, append it and its precision score
+            clf = self.estimator()
+            X_train, y_train = X[train_index], y[train_index]
             X_test, y_test = X[test_index], y[test_index]
 
-            for train_index2, test_index2 in folds.split(X_test):  # second folding
-                X_train2, y_train2 = X[train_index2], y[train_index2]
-                X_test2, y_test2 = X[test_index2], y[test_index2]
+            clf.fit(X_train, y_train)
+            y_prediction = clf.predict(X_test)
+            score = precision_score(y_test, y_prediction, average='micro')
 
-                self.estimator.fit(X_train2, y_train2)
-                y_pred = self.estimator.predict(X_test2)
-                prediction_vectors.append(y_pred)
-                score = precision_score(y_test2, y_pred, average='micro')
-                p_scores.append(score)
+            self.classifiers_group.append(clf)
+            self.precision_scores.append(score)
 
-        return prediction_vectors, p_scores
+    def predict(self, X):
+        prediction_vectors = self.estimate_individual_predictions(X)
+        max_size = self.get_max_size(prediction_vectors)
+        decision_data_sets = self.accumulate_predictions(prediction_vectors, self.precision_scores, max_size)
+        results = self.calculate_final_decisions(decision_data_sets)
+        return results
 
-    def get_max_size(self, data):
+    def estimate_individual_predictions(self, X):
+        prediction_vectors = []
+
+        for clf in self.classifiers_group:
+            y_prediction = clf.predict(X)
+            prediction_vectors.append(y_prediction)
+
+        return prediction_vectors
+
+    def get_max_size(self, data): # get maxsize (because data not always splits evenly)
         max = 0
         for vector in data:
             if max < len(vector):
                 max = len(vector)
         return max
 
-    def pre_process_data(self, data):
-        return PCA().fit_transform(StandardScaler().fit_transform(data))
-
     def accumulate_predictions(self, prediction_vectors, p_scores, max_size):
         decision_data_sets = []
 
-        for i in range(max_size):
+        for i in range(max_size): #for each element in vectors
             decision_data_sets.append(DecisionDataSet())
 
-            for k, vector in enumerate(prediction_vectors):
+            for k, vector in enumerate(prediction_vectors): #for each vector
                 if len(vector) > 0:
                     prediction, vector = vector[0], vector[1:]  # pop first element
                     if prediction in decision_data_sets[i].class_occurrence:  # check if class is in class occurrence
@@ -84,9 +80,9 @@ class DiversifiedClassifier:
 
         for dds in decision_data_sets:
             classes = []
-            max_value = 0
+            max_value = 0 #the biggest class occurence * class precision score
 
-            for key in dds.class_occurrence.keys():
+            for key in dds.class_occurrence.keys(): #for each class in the voting
                 if (dds.class_occurrence[key] * dds.class_precision[key]) > max_value:
                     classes.clear()
                     classes.append(key)
--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 # Estimators
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
 # Others
 from numpy import genfromtxt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+
 from diversified_classifier import DiversifiedClassifier
-from standard_classifier import StandardClassifier
 
-
+n_splits = 5  # configure n of folds
+div_clf = DiversifiedClassifier(KNeighborsClassifier(), n_splits)
+std_clf = KNeighborsClassifier()
 for i in range(1, 11):
     data = genfromtxt('./data/classification_%d.csv' % i, delimiter=",")
+    X = data[:, :-1]
+    y = data[:, -1]
+    folds = KFold(n_splits=n_splits)
 
-    std_clf = StandardClassifier(KNeighborsClassifier())
-    div_clf = DiversifiedClassifier(KNeighborsClassifier(), 3)
+    for train_index, test_index in folds.split(X):
+        X_train, y_train = X[train_index], y[train_index]
+        X_test, y_test = X[test_index], y[test_index]
 
-    print(std_clf.predict(data))
-    print(div_clf.predict(data))
+        div_clf.fit(X_train, y_train)
+        div_pred = div_clf.predict(X_test)
+        div_score = precision_score(y_test, div_pred, average='micro')
 
+        std_clf.fit(X_train, y_train)
+        std_pred = std_clf.predict(X_test)
+        std_score = precision_score(y_test, std_pred, average='micro')
 
-
+        #tutaj porownanie jakie nam bedzie potrzebne
+        if std_score > div_score:
+            print('STD!')
+        else:
+            print('DIV!')
