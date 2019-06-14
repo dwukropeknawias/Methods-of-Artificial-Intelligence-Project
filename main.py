0f53a77f6d201a81cfce8bdc0a7a02aaa70e3e8f
mbanaszkiewicz <baan616@gmail.com>
2019-06-14 17:52:38 +0200

    different comparison

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Estimators
 # Others
 from numpy import genfromtxt
+from numpy import concatenate
 from sklearn.metrics import precision_score
 from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
@@ -16,20 +17,23 @@ for i in range(1, 11):
     y = data[:, -1]
     folds = KFold(n_splits=n_splits)
 
+    std_preds = []
+    div_preds = []
     for train_index, test_index in folds.split(X):
         X_train, y_train = X[train_index], y[train_index]
         X_test, y_test = X[test_index], y[test_index]
 
         div_clf.fit(X_train, y_train)
-        div_pred = div_clf.predict(X_test)
-        div_score = precision_score(y_test, div_pred, average='micro')
+        div_preds.append( div_clf.predict(X_test))
+
 
         std_clf.fit(X_train, y_train)
-        std_pred = std_clf.predict(X_test)
-        std_score = precision_score(y_test, std_pred, average='micro')
+        std_preds.append( std_clf.predict(X_test))
 
-        #tutaj porownanie jakie nam bedzie potrzebne
-        if std_score > div_score:
-            print('STD!')
-        else:
-            print('DIV!')
+    std_pred = concatenate(std_preds, axis=0)
+    div_pred = concatenate(div_preds, axis=0)
+
+    div_score = precision_score(y, div_pred, average='micro')
+    std_score = precision_score(y, std_pred, average='micro')
+
+    print(f'Dataset: {i}. \n STD precisions score:{std_score} \n DIV Precision Score:{div_score}\n')
